9849b800916f4a34b13107984b6400bca68b5406
Deon <dsimmons@sjcoe.net>
2021-11-10 15:58:42 -0800

    Mobile version for home page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
+//import '@fortawesome/fontawesome-free/js/all.js';
+//import 
 
 @Component({
   selector: 'app-header',
@@ -10,11 +14,17 @@ export class HeaderComponent implements OnInit {
 
   constructor(private router: Router) { }
 
+  public isMenuCollapsed = true;
+  btnClicked: boolean = true;
+
   ngOnInit(): void {
   }
 
+  goHome(){
+    this.router.navigate(['home']);
+  }
   getConnected() {
-    this.router.navigate(['signup'])
+    this.router.navigate(['signup']);
   }
 
 }
